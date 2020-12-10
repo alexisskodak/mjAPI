34115d8ddb9c5429d16a248f98ba025deac108f8
alexis <skodak95alexis@gmail.com>
2020-12-10 15:28:21 +0100

    Updated requirements.txt, added heroku config for deploy

--- MJ/settings.py
+++ MJ/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import environ
-import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
