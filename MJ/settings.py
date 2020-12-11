f99c40afcf6c8a3bdfe18de2048d73f25bae206b
alexis <skodak95alexis@gmail.com>
2020-12-11 12:05:35 +0100

    Added REST_FRAMEWORK Token Auth

--- MJ/settings.py
+++ MJ/settings.py
@@ -50,11 +50,12 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', )
 }
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
