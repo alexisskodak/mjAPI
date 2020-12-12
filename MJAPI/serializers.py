ff31076ec851328ff728c0edb434fbac3602d722
alexis <skodak95alexis@gmail.com>
2020-12-12 17:25:27 +0100

    Implements create method for nested Contact object

--- MJAPI/serializers.py
+++ MJAPI/serializers.py
@@ -15,6 +15,12 @@ class RetailerSerializer(serializers.ModelSerializer):
         model = Retailer
         fields = '__all__'
 
+    def create(self, validated_data):
+        contact_data = validated_data.pop('contact')
+        retailer = Retailer.objects.create(**validated_data)
+        Contact.objects.create(retailer=retailer, **contact_data)
+        return retailer
+
 
 class DeliveryPartnerSerializer(serializers.ModelSerializer):
     contact = ContactSerializer()
@@ -23,14 +29,26 @@ class DeliveryPartnerSerializer(serializers.ModelSerializer):
         model = DeliveryPartner
         fields = '__all__'
 
+    def create(self, validated_data):
+        contact_data = validated_data.pop('contact')
+        delivery_partner = DeliveryPartner.objects.create(**validated_data)
+        Contact.objects.create(delivery_partner=delivery_partner, **contact_data)
+        return delivery_partner
+
 
 class CustomerSerializer(serializers.ModelSerializer):
     contact = ContactSerializer()
-    favoriteShops = RetailerSerializer()
+    favoriteShops = RetailerSerializer(many=True)
 
     class Meta:
         model = Customer
         fields = '__all__'
+
+    def create(self, validated_data):
+        contact_data = validated_data.pop('contact')
+        customer = Customer.objects.create(**validated_data)
+        Contact.objects.create(customer=customer, **contact_data)
+        return customer
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -40,7 +58,7 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer()
+    category = CategorySerializer(many=True)
     retailer = RetailerSerializer()
 
     class Meta:
@@ -50,7 +68,7 @@ class ProductSerializer(serializers.ModelSerializer):
 
 class OrderSerializer(serializers.ModelSerializer):
     customer = CustomerSerializer()
-    product = ProductSerializer()
+    product = ProductSerializer(many=True)
     retailer = RetailerSerializer()
     deliveryPartner = DeliveryPartnerSerializer()
 
