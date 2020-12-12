3660b653d07fb03820835b42c1173681ba385132
alexis <skodak95alexis@gmail.com>
2020-12-12 16:19:28 +0100

    Adds DEBUG=TRUE for testing

--- MJ/settings.py
+++ MJ/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env(env_file='db.env')
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
