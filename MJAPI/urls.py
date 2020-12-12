057250b03eb448ecd2ccf8593b59b8ff8e0eaffe
alexis <skodak95alexis@gmail.com>
2020-12-12 12:44:08 +0100

    Implements DRF Router instead of Django urlpatterns

--- MJAPI/urls.py
+++ MJAPI/urls.py
@@ -1,71 +1,13 @@
-from django.urls import path
+from rest_framework import routers
 from .views import *
 
-urlpatterns = [
-    path('contacts/', ContactViewSet.as_view(
-        {
-            'get': 'list',
-            'post': 'create'
-        })),
-    path('contacts/<int:pk>/', ContactViewSet.as_view(
-        {
-            'get': 'retrieve',
-            'put': 'update',
-            'delete': 'destroy'
-        })),
-    path('retailers/', RetailerViewSet.as_view(
-        {
-            'get': 'list',
-            'post': 'create'
-        })),
-    path('retailers/<int:pk>/', RetailerViewSet.as_view(
-        {
-            'get': 'retrieve',
-            'put': 'update',
-            'delete': 'destroy'
-        })),
-    path('delivery_partners/', DeliveryPartnerViewSet.as_view(
-        {
-            'get': 'list',
-            'post': 'create'
-        })),
-    path('delivery_partners/<int:id>/', DeliveryPartnerViewSet.as_view(
-        {
-            'get': 'retrieve',
-            'put': 'update',
-            'delete': 'destroy'
-        })),
-    path('customers/', CustomerViewSet.as_view(
-        {
-            'get': 'list',
-            'post': 'create'
-        })),
-    path('customers/<int:id>/', CustomerViewSet.as_view(
-        {
-            'get': 'retrieve',
-            'put': 'update',
-            'delete': 'destroy'
-        })),
-    path('products/', ProductViewSet.as_view(
-        {
-            'get': 'list',
-            'post': 'create'
-        })),
-    path('products/<int:id>/', ProductViewSet.as_view(
-        {
-            'get': 'retrieve',
-            'put': 'update',
-            'delete': 'destroy'
-        })),
-    path('orders/', OrderViewSet.as_view(
-        {
-            'get': 'list',
-            'post': 'create'
-        })),
-    path('orders/<int:id>/', OrderViewSet.as_view(
-        {
-            'get': 'retrieve',
-            'put': 'update',
-            'delete': 'destroy'
-        })),
-]
+router = routers.DefaultRouter()
+
+router.register('contacts', viewset=ContactViewSet)
+router.register('retailers', viewset=RetailerViewSet)
+router.register('delivery-partners', viewset=DeliveryPartnerViewSet)
+router.register('customers', viewset=CustomerViewSet)
+router.register('products', viewset=ProductViewSet)
+router.register('orders', viewset=OrderViewSet)
+
+urlpatterns = router.urls
